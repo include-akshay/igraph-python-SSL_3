59fb531ed218c5ef9e6c41ce0022d4b5daa3b5ab
Nitesh Dohre <niteshdohre@gmail.com>
2022-11-06 19:23:00 +0530

    Greedy coloring function is created and defined...

--- igraph/greedy_chromatic_number.py
+++ igraph/greedy_chromatic_number.py
@@ -25,7 +25,37 @@ class chromatic_number_greedy:
 
 # Assigns colors (starting from 0) to all
 # vertices and prints the assignment of colors
+def greedyColoring(self):
+        
+        result = [-1] * self.V
 
+       
+        result[0] = 0
+
+
+        
+        available = [False] * self.V
+
+        for u in range(1, self.V):
+            for i in self.adj[u]:
+                if (result[i] != -1):
+                    available[result[i]] = True
+            color = 0
+            while color < self.V:
+                if (available[color] == False):
+                    break
+                
+                color += 1
+                
+            # Assign the found color
+            result[u] = color
+            for i in self.adj[u]:
+                if (result[i] != -1):
+                    available[result[i]] = False
+
+        # Print the result
+        for u in range(self.V):
+            print("Vertex", u, " ---> Color", result[u])
 
    
 
