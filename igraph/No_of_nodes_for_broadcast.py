d44d373d57c2442b1a6b2e92806210640c2f1350
include-akshay <akshaypratapsingh46@gmail.com>
2022-11-06 10:34:49 +0530

    Initialized a file in the library and defined __init__ alongwith an utility method

--- igraph/No_of_nodes_for_broadcast.py
+++ igraph/No_of_nodes_for_broadcast.py
@@ -1,10 +1,11 @@
 from collections import defaultdict
 class Number_of_sources:
   
+    # to iniialize the class object with the number of vertices asked by the user and also create an adjacency list
     def __init__(self,vertices):
         self.V= vertices 
         self.graph = defaultdict(list)
         
-
+    # to add edges provided by the user in the adjacancy list
     def addEdge(self,u,v):
         self.graph[u].append(v)
