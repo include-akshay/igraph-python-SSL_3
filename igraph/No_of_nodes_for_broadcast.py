c77d85a12176effc38cfd26dacf4edf289c55dbb
include-akshay <akshaypratapsingh46@gmail.com>
2022-11-06 10:44:43 +0530

    Created a method : reverse_graph

--- igraph/No_of_nodes_for_broadcast.py
+++ igraph/No_of_nodes_for_broadcast.py
@@ -10,8 +10,10 @@ class Number_of_sources:
     def addEdge(self,u,v):
         self.graph[u].append(v)
 
-    """this is inorder to perform DFS on the given graph and push the nodes in the stack according to their post order numbering
-    and which will be used later on to convert the provided graph with it's reverse graph"""
+    """this is inorder to perform DFS on the given graph and push the nodes 
+    in the stack according to their post order numbering
+    and which will be used later on to convert the provided graph with it's 
+    reverse graph"""
     def explore(self,v,visited, stack):
         visited[v]= True
         for i in self.graph[v]:
@@ -19,3 +21,18 @@ class Number_of_sources:
                 self.explore(i, visited, stack)
 
         stack = stack.append(v)
+
+
+    """This is method which is used to reverse graph G=(V, E) to G=(V',E'). 
+    Such that for every edge (u,v) in E, there will exist an edge(v,u) in 
+    E' in G'.
+    This way we can have reverse graph and will be used later on to decide the 
+    requirements of the class"""
+    def reverse_graph(self):
+        g = Number_of_sources(self.V)
+ 
+
+        for i in self.graph:
+            for j in self.graph[i]:
+                g.addEdge(j,i)
+        return g
