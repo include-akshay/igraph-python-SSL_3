16302c47f705ace13a7999207f3e5086fa5889ba
a_k_sharma <anurag@pop-os.localdomain>
2022-11-08 14:48:44 +0530

    added test case for No of Nodes for broadcast

--- igraph/tests/test_mimimum_frequency_bands.py
+++ igraph/tests/test_mimimum_frequency_bands.py
@@ -4,12 +4,8 @@ from collections import defaultdict
 sys.path.append('/home/anurag/work/ass3/ass3/igraph-python-SSL_3/igraph')
 import minimum_frequency_bands
 graph = minimum_frequency_bands.minimum_frequency_bands(5)
-graph.addEdge(0, 1)
-graph.addEdge(0, 2)
-graph.addEdge(1, 2)
-graph.addEdge(1, 3)
-graph.addEdge(2, 3)
-graph.addEdge(3, 4)
+
+
 
 class AttributeTest(unittest.TestCase):
     def testaddEdge(self):
--- igraph/tests/test_no_of_nodes_for_broadcast.py
+++ igraph/tests/test_no_of_nodes_for_broadcast.py
@@ -1,3 +1,26 @@
 import sys
 import unittest
+from collections import defaultdict
+sys.path.append('/home/anurag/work/ass3/ass3/igraph-python-SSL_3/igraph')
+import No_of_nodes_for_broadcast
+
+graph = No_of_nodes_for_broadcast.Number_of_sources(5)
+
+graph.addEdge(0, 1)
+graph.addEdge(0, 2)
+graph.addEdge(1, 2)
+graph.addEdge(1, 3)
+graph.addEdge(2, 3)
+graph.addEdge(3, 4)
+
+
+class AttributeTest(unittest.TestCase):
+    def testreverse_graph(self):
+        reverse_list = graph.reverse_graph()
+        self.assertDictEqual(reverse_list.__dict__['graph'],{1: [0], 2: [0, 1], 3: [1, 2], 4: [3]},"Unequal dict")
+
+
+ 
+
+
 
